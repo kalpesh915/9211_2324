caca4cc1e1a7da1228cb57ce88f4827801d5f440
Kalpesh Chauhan <63454815+kalpesh915@users.noreply.github.com>
2023-08-12 09:49:15 +0530

    12-8

---
+++ C Language/pro77.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void main(){
+    printf("\n welcome to \n world of \n \"C Language\"");
+}
---
+++ C Language/pro78.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+    int x = 15; /// 15 is constant
+
+    printf("\n value of x is %d", x);
+    x = 010; /// octal
+    printf("\n value of x is %d", x);
+    x = 0x17; /// hex decimal
+    printf("\n value of x is %d", x);
+}
