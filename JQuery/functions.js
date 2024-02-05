b099c18e39260496c0940e0ea87d585cfe61419d
Kalpesh Chauhan <63454815+kalpesh915@users.noreply.github.com>
2024-02-05 10:10:14 +0530

    JQuery Day 1

---
+++ JQuery/functions.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+        $("#btn1").click(function(){
+            $("#op").hide(2000);
+        });
+
+        $("#btn2").click(function(){
+            $("#op").show(2000);
+        });
+    });
